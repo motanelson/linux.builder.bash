193217acdb9d6394657c435a547960ec8c9b4562
nelson jr mota <118323821+motanelson@users.noreply.github.com>
2026-01-12 08:01:34 +0000

    Add files via upload

--- builder.sh
+++ builder.sh
@@ -6,17 +6,21 @@ read g
 dd if=/dev/zero of=$h bs=1M count=$g status=progress
 chmod 777 $h
 echo '.........................................'
-mkfs.ext2 -F $h
+mkfs.fat -F 12 $h
 chmod 777 $h
 echo '.........................................'
-
+mkdir /mnt/rams 2>/dev/null
+sudo umount /mnt/rams 2>/dev/null
+sudo mount -o loop $h /mnt/rams
 echo '.........................................'
 printf "\033[40;37m\ngive me the files to include in main root ? "
 read a
 for b in $a
 do
-    mcopy -i $h $b ::/$b
+    sudo cp $b /mnt/rams
+    sudo chmod 777 /mnt/rams/$b
 done
+sudo umount /mnt/rams 2>/dev/null
 echo '.........................................'
 g=14
 h=boot.img
