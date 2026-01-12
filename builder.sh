659506863d914e5a55683d2599810c6669423070
nelson jr mota <118323821+motanelson@users.noreply.github.com>
2026-01-12 07:14:01 +0000

    Add files via upload

--- builder.sh
+++ builder.sh
@@ -1,24 +1,25 @@
 printf "\033c\n "
+printf "\033[40;37m\ngive me disk image name ? "
+read h
+printf "\033[40;37m\ngive me disk image size in MB ? "
+read g
+dd if=/dev/zero of=$h bs=1M count=$g status=progress
+chmod 777 $h
+echo '.........................................'
+mkfs.ext2  $h
+chmod 777 $h
+echo '.........................................'
 
-                umount /mnt/rams 2> /dev/null
-                rm initrd 2> /dev/null
-                cp init initrd
-                mkdir /mnt/rams 2> /dev/null
-                mount -o loop initrd /mnt/rams
 echo '.........................................'
 printf "\033[40;37m\ngive me the files to include in main root ? "
 read a
 for b in $a
 do
-    cp $b /mnt/rams
+    mcopy -i $h $b ::/$b
 done
 echo '.........................................'
-chmod 777 /mnt/rams/*
-umount /mnt/rams  2> /dev/null
-printf "\033[40;37m\ngive me disk image size in MB ? "
-read g
-printf "\033[40;37m\ngive me disk image name ? "
-read h
+g=14
+h=boot.img
 dd if=/dev/zero of=$h bs=1M count=$g status=progress
 chmod 777 $h
 echo '.........................................'
@@ -26,9 +27,6 @@ mkfs.fat -F 12 $h
 chmod 777 $h
 echo '.........................................'
 syslinux  $h
-chmod 777 $h
-rm initrd.gz 2> /dev/null
-gzip initrd 2> /dev/null
 chmod 777 $h
 echo '.........................................'
 mcopy -i $h syslinux.cfg ::/syslinux.cfg
